b5f71f6ec12148fb2d3da8874789a725e56fbc51
Default Author <default@example.com>
2025-05-08 12:36:36 -0400

    Project import generated by Copybara.
    GitOrigin-RevId: ca87de994716cefdb19e079d4023f1b446f5f51b

--- services/tracker/src/main/kotlin/ChatCompletionClientFactory.kt
+++ services/tracker/src/main/kotlin/ChatCompletionClientFactory.kt
@@ -1,19 +1,21 @@
 package com.trash.services.tracker
 
+import io.micronaut.context.annotation.Value
 import jakarta.inject.Singleton
 
 @Singleton
 class ChatCompletionClientFactory(
-  private val openAIClient: OpenAIClient,
-  private val geminiClient: GeminiClient
+    private val openAIClient: OpenAIClient,
+    private val geminiClient: GeminiClient,
+    @Value("\${chat.env}")
+    private val environment: String
 ) {
-  fun getClient(model: String): ChatCompletionClient {
-    return when (model.lowercase()) {
-      "openai" -> openAIClient
-      "gemini" -> geminiClient
-      else -> {
-        geminiClient
-      }
+    fun getClient(model: String): ChatCompletionClient {
+        return when {
+            environment.lowercase() == "test" -> openAIClient
+            model.lowercase() == "openai" -> openAIClient
+            model.lowercase() == "gemini" -> geminiClient
+            else -> geminiClient
+        }
     }
-  }
 }
